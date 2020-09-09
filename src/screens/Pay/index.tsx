007e4543a5255780d323d3f545a5bf1744c2f956
zevdvlpr <zevdvlpr@gmail.com>
2020-09-09 11:20:45 -0300

    chore: screens

---
+++ src/screens/Pay/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const Pay: React.FC = () => {
+  return (
+    <Container>
+      <Text>Pay</Text>
+    </Container>
+  );
+};
+
+export default Pay;
---
+++ src/screens/Wallet/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const Wallet: React.FC = () => {
+  return (
+    <Container>
+      <Text>Wallet</Text>
+    </Container>
+  );
+};
+
+export default Wallet;
